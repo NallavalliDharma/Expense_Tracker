77c6ca4ac0fd044032b95b24e1f3dd1d4b1f8aec
Nallavalli Dharma <nallavallidharma@gmail.com>
2025-08-29 23:36:14 +0530

    Initial commit - Expense Tracker project

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { verifyOtp } = require("../controllers/authController");
+const { protect } = require("../middleware/authMiddleware");
+const {
+  registerUser,
+  loginUser,
+  getUserInfo,
+} = require("../controllers/authController");
+const upload = require("../middleware/uploadMiddleware");
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/verify-otp", verifyOtp);
+router.get("/getUser", protect, getUserInfo);
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+
+  // Use BACKEND_URL from env, fallback to request protocol+host (for localhost testing)
+  const serverUrl = process.env.BACKEND_URL || `${req.protocol}://${req.get("host")}`;
+  const imageUrl = `${serverUrl}/uploads/${req.file.filename}`;
+
+  console.log("Uploaded image URL: ",imageUrl);
+  res.status(200).json({ imageUrl });
+});
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+const dashboardRoutes = require('./routes/dashboardRoutes');
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// CORS setup
+const allowedOrigins = [process.env.CLIENT_URL, "http://localhost:5173"];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    console.log("Incoming origin:", origin);
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log("Blocked by CORS:", origin);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
+
+// Static uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
